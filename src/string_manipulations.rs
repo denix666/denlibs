0a528fb1950c4c3717942455e1dde417156c47e9
Denis Salmanovich <dsalmanovich@eisgroup.com>
2023-06-28 16:27:05 +0300

    version 0.1.8

--- src/string_manipulations.rs
+++ src/string_manipulations.rs
@@ -1,17 +1,46 @@
-
 /// Removes double quota of the string passed
 /// 
 /// # Example
 /// ```rust
 /// use denlibs::string_manipulations::remove_double_quotas;
 /// 
-/// fn main() {
-///     println!("String with removed double quotas: {}", remove_double_quotas("\"test\"".to_string()));
-/// }
+/// println!("String with removed double quotas: {}", remove_double_quotas("\"test\"".to_string()));
 /// ```
 pub fn remove_double_quotas(mut string: String) -> String {
     string.retain(|c| c != '"');
     return string
+}
+
+/// Function for searching plain-text data sets for lines that match a patterns in a provided file.
+/// 
+/// # Example
+/// ```rust
+/// use denlibs::string_manipulations::grep;
+/// 
+/// let path_buf = std::path::PathBuf::from("/tmp/my_file");
+/// let path = path_buf.as_os_str();
+/// 
+/// if let Ok(result) = grep(path, "my text") {
+///     println!("Found line: {}", result);
+/// }
+/// 
+/// ```
+pub fn grep(path_to_file: &std::ffi::OsStr, search_text: &str) -> Result<String, Box<dyn std::error::Error>> {
+    match std::fs::File::open(path_to_file) {
+        Ok(file) => {
+            let reader = std::io::BufReader::new(file);
+            for line in std::io::BufRead::lines(reader) {
+                let res: String = line.unwrap();
+                if res.contains(search_text) {
+                    return Ok(res.to_string())
+                }
+            }
+            Err("Searching text not found".into())
+        },
+        Err(_) => {
+            return Err("Unable to open the file.".into())
+        }
+    }
 }
 
 #[cfg(test)]
